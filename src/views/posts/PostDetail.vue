ab4b83b2b6afa81b5d0f1dff318c0dbb97b44c8a
mysoyul <vega2k_@naver.com>
2024-05-14 15:31:04 +0900

    posts view 컴포넌트추가

---
+++ src/views/posts/PostDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>PostDetail</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
